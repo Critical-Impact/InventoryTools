fb7814360b50191cc0ac029f9b7a9d34ba7c65ca
Dominic Renner <Rennerdo30@users.noreply.github.com>
2022-01-08 19:09:27 +0100

    fix sorting

--- InventoryTools/Logic/Columns/MarketBoardPriceColumn.cs
+++ InventoryTools/Logic/Columns/MarketBoardPriceColumn.cs
@@ -14,25 +14,38 @@ namespace InventoryTools.Logic
         public string Name { get; set; } = "MB Current Average Price";
         public float Width { get; set; } = 250.0f;
         public string FilterText { get; set; } = "";
-        
+
+        private static readonly string LOADING = "loading...";
+
+        private string Value(InventoryItem item)
+        {
+            var marketBoardData = Universalis.GetMarketBoardData(item, Service.ClientState.LocalPlayer.CurrentWorld.GameData.Name.RawString);
+            if (marketBoardData != null)
+            {
+                return $"{marketBoardData.currentAveragePrice}";
+            }
+
+            return LOADING;
+        }
+
         public IEnumerable<InventoryItem> Filter(IEnumerable<InventoryItem> items)
         {
-            return FilterText == "" ? items : items.Where(c => c.FormattedName.ToLower().PassesFilter(FilterText.ToLower()));
+            return FilterText == "" ? items : items.Where(c => Value(c).ToLower().PassesFilter(FilterText.ToLower()));
         }
 
         public IEnumerable<SortingResult> Filter(IEnumerable<SortingResult> items)
         {
-            return FilterText == "" ? items : items.Where(c => c.InventoryItem.FormattedName.ToLower().PassesFilter(FilterText.ToLower()));
+            return FilterText == "" ? items : items.Where(c => Value(c.InventoryItem).ToLower().PassesFilter(FilterText.ToLower()));
         }
 
         public IEnumerable<InventoryItem> Sort(ImGuiSortDirection direction, IEnumerable<InventoryItem> items)
         {
-            return direction == ImGuiSortDirection.Ascending ? items.OrderBy(c => c.FormattedName.ToLower()) : items.OrderByDescending(c => c.FormattedName.ToLower());
+            return direction == ImGuiSortDirection.Ascending ? items.OrderBy(c => Value(c)) : items.OrderByDescending(c => Value(c));
         }
 
         public IEnumerable<SortingResult> Sort(ImGuiSortDirection direction, IEnumerable<SortingResult> items)
         {
-            return direction == ImGuiSortDirection.Ascending ? items.OrderBy(c => c.InventoryItem.FormattedName.ToLower()) : items.OrderByDescending(c => c.InventoryItem.FormattedName.ToLower());
+            return direction == ImGuiSortDirection.Ascending ? items.OrderBy(c => Value(c.InventoryItem)) : items.OrderByDescending(c => Value(c.InventoryItem));
         }
 
         public void Draw(InventoryItem item)
@@ -45,19 +58,20 @@ namespace InventoryTools.Logic
         {
             ImGui.TableNextColumn();
 
-            var marketBoardData = Universalis.GetMarketBoardData(item.InventoryItem, Service.ClientState.LocalPlayer.CurrentWorld.GameData.Name.RawString);
-            if (marketBoardData != null)
+            var marketBoardData = Value(item.InventoryItem);
+            if (marketBoardData != LOADING)
             {
-                ImGui.TextColored(item.InventoryItem.ItemColour, $"{marketBoardData.currentAveragePrice}");
-            } else
+                ImGui.TextColored(item.InventoryItem.ItemColour, $"{marketBoardData}");
+            }
+            else
             {
-                ImGui.TextColored(ImGuiColors.DalamudYellow, $"loading...");
+                ImGui.TextColored(ImGuiColors.DalamudYellow, LOADING);
             }
         }
 
         public void Setup(int columnIndex)
         {
-            ImGui.TableSetupColumn(Name, ImGuiTableColumnFlags.WidthFixed, Width,(uint)columnIndex);
+            ImGui.TableSetupColumn(Name, ImGuiTableColumnFlags.WidthFixed, Width, (uint)columnIndex);
         }
     }
 }
