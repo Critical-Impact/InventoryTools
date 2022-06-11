841646f3c1a0a3851830850eb4adde72a0753789
Critical Goatpact <criticalimpact@gmail.com>
2022-06-11 13:01:50 +0100

    Fix display issue with FC chest

--- InventoryTools/GameUi/FreeCompanyChestOverlay.cs
+++ InventoryTools/GameUi/FreeCompanyChestOverlay.cs
@@ -61,7 +61,7 @@ namespace InventoryTools.GameUi
         {
             for (int x = 0; x < 5; x++)
             {
-                for (int y = 0; y < 7; y++)
+                for (int y = 0; y < 10; y++)
                 {
                     EmptyDictionary.Add(new Vector2(x,y), null);
                 }
