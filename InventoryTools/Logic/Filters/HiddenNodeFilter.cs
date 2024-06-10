6c46d786b0965c000a90bd90dffd166a3de06b80
Critical <criticalimpact@gmail.com>
2024-06-10 18:48:21 +1000

    **New Features** Added Ephemeral & Hidden Node columns/filters
    **Bug Fixes**
    Certain items were showing as being collected from ephemeral nodes when they were not
    Columns in the columns/craft columns picker will be in alphabetical order

---
+++ InventoryTools/Logic/Filters/HiddenNodeFilter.cs
@@ -0,0 +1,39 @@
+using CriticalCommonLib.Models;
+using CriticalCommonLib.Sheets;
+using InventoryTools.Logic.Filters.Abstract;
+using InventoryTools.Services;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryTools.Logic.Filters
+{
+    public class HiddenNodeFilter : BooleanFilter
+    {
+        public override string Key { get; set; } = "HiddenNode";
+        public override string Name { get; set; } = "Is Hidden Node?";
+        public override string HelpText { get; set; } = "Is the item available in hidden nodes?";
+        public override FilterType AvailableIn { get; set; }  = FilterType.SearchFilter | FilterType.SortingFilter | FilterType.GameItemFilter | FilterType.HistoryFilter;
+        public override FilterCategory FilterCategory { get; set; } = FilterCategory.Gathering;
+
+        public override bool? FilterItem(FilterConfiguration configuration,InventoryItem item)
+        {
+            return FilterItem(configuration, item.Item);
+        }
+
+        public override bool? FilterItem(FilterConfiguration configuration, ItemEx item)
+        {
+            var currentValue = CurrentValue(configuration);
+            if (currentValue == null) return true;
+            
+            if(currentValue.Value && item.IsItemAvailableAtHiddenNode)
+            {
+                return true;
+            }
+                
+            return !currentValue.Value && !item.IsItemAvailableAtHiddenNode;
+        }
+
+        public HiddenNodeFilter(ILogger<HiddenNodeFilter> logger, ImGuiService imGuiService) : base(logger, imGuiService)
+        {
+        }
+    }
+}
