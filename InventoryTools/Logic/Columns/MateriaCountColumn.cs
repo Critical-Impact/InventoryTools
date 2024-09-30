5e7951b582b0e398cecb8d4f0a8316a712bf44ee
Critical Impact <criticalimpact@gmail.com>
2024-09-30 19:17:15 +1000

    Update new columns

--- InventoryTools/Logic/Columns/MateriaCountColumn.cs
+++ InventoryTools/Logic/Columns/MateriaCountColumn.cs
@@ -16,21 +16,11 @@ public class MateriaCountColumn : IntegerColumn
     public override float Width { get; set; } = 90;
     public override string HelpText { get; set; } = "How many materia does this item have or can it have?";
     public override ColumnCategory ColumnCategory { get; } = ColumnCategory.Basic;
+    public override int? CurrentValue(ColumnConfiguration columnConfiguration, SearchResult searchResult)
+    {
+        return searchResult.InventoryItem?.MateriaCount ?? searchResult.Item.MateriaSlotCount;
+    }
+
     public override bool HasFilter { get; set; } = true;
     public override ColumnFilterType FilterType { get; set; } = ColumnFilterType.Text;
-
-    public override int? CurrentValue(ColumnConfiguration columnConfiguration, InventoryItem item)
-    {
-        return item.MateriaCount;
-    }
-
-    public override int? CurrentValue(ColumnConfiguration columnConfiguration, ItemEx item)
-    {
-        return item.MateriaSlotCount;
-    }
-
-    public override int? CurrentValue(ColumnConfiguration columnConfiguration, SortingResult item)
-    {
-        return CurrentValue(columnConfiguration, item.InventoryItem);
-    }
 }
